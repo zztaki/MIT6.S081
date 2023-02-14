71fc0506cbc06347defc9b23f936e1ef87ce8de4
zztaki <1208191544@qq.com>
2023-02-14 16:24:34 +0800

    complete trap

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -88,3 +88,58 @@ sys_uptime(void) {
     release(&tickslock);
     return xticks;
 }
+
+uint64 sys_sigalarm(void) {
+    int ticks;
+    uint64 handler;
+
+    if (argint(0, &ticks) < 0 || argaddr(1, &handler) < 0)
+        return -1;
+
+    struct proc *p = myproc();
+    p->last_ticks = 0;
+    p->ticks = ticks;
+    p->handler = (void (*)())handler;
+    p->can_enter_alarm = 1;
+    return 0;
+}
+
+uint64 sys_sigreturn(void) {
+    struct proc *p = myproc();
+
+    p->trapframe->epc = p->his_epc;
+    p->trapframe->ra = p->his_ra;
+    p->trapframe->sp = p->his_sp;
+    p->trapframe->gp = p->his_gp;
+    p->trapframe->tp = p->his_tp;
+    p->trapframe->a0 = p->his_a0;
+    p->trapframe->a1 = p->his_a1;
+    p->trapframe->a2 = p->his_a2;
+    p->trapframe->a3 = p->his_a3;
+    p->trapframe->a4 = p->his_a4;
+    p->trapframe->a5 = p->his_a5;
+    p->trapframe->a6 = p->his_a6;
+    p->trapframe->a7 = p->his_a7;
+    p->trapframe->t0 = p->his_t0;
+    p->trapframe->t1 = p->his_t1;
+    p->trapframe->t2 = p->his_t2;
+    p->trapframe->t3 = p->his_t3;
+    p->trapframe->t4 = p->his_t4;
+    p->trapframe->t5 = p->his_t5;
+    p->trapframe->t6 = p->his_t6;
+    p->trapframe->s0 = p->his_s0;
+    p->trapframe->s1 = p->his_s1;
+    p->trapframe->s2 = p->his_s2;
+    p->trapframe->s3 = p->his_s3;
+    p->trapframe->s4 = p->his_s4;
+    p->trapframe->s5 = p->his_s5;
+    p->trapframe->s6 = p->his_s6;
+    p->trapframe->s7 = p->his_s7;
+    p->trapframe->s8 = p->his_s8;
+    p->trapframe->s9 = p->his_s9;
+    p->trapframe->s10 = p->his_s10;
+    p->trapframe->s11 = p->his_s11;
+
+    p->can_enter_alarm = 1;
+    return 0;
+}
